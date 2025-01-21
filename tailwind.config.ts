933d5218313ad15d732a34cf4cf67bef697fc23e
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-01-21 10:02:52 +0100

    Initial setup with Next.js, Tailwind CSS, and ShadCN components

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import { Config } from "tailwindCSS";
+
+export default {
+	darkMode: ["class"],
+	content: [
+	  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+	  extend: {
+		colors: {
+		  primary: {
+			DEFAULT: '#A31D1D', 
+			foreground: '#ffffff', 
+		  },
+		  background: '#A31D1D', 
+		  foreground: '#ffffff',  
+		},
+	  },
+	},
+	plugins: [require("tailwindcss-animate")],
+  } satisfies Config;
+  
