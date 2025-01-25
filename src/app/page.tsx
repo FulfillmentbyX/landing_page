858d333de220cf55b9d4be9f778dd4b509242151
Chinedu Ekene Okpala <allstackdev@gmail.com>
2025-01-25 11:32:35 +0000

    configure netlify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button";
 import { LandingPage } from "@/app/landing-page/LandingPage";
-
 
 export default function Home() {
   return (
     <div>
-
-
     <LandingPage/>
     </div>
     
