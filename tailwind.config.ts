e8e67f9984ec03941dc6bafbd4fa257eea45171d
Chinedu Ekene Okpala <allstackdev@gmail.com>
2025-01-25 12:20:22 +0000

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,25 @@
-import { Config } from "tailwindCSS";
+import type { Config } from "tailwindCSS";
+import tailwindcssAnimate from 'tailwindcss-animate';
 
+/** @type {Config} */
 export default {
-	darkMode: ["class"],
-	content: [
-	  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-	  extend: {
-		colors: {
-		  primary: {
-			DEFAULT: '#A31D1D', 
-			foreground: 'color: black;', 
-		  },
-		  background: '#A31D1D', 
-		  foreground: '#ffffff',  
-		},
-	  },
-	},
-	plugins: [require("tailwindcss-animate")],
-  } satisfies Config;
-  
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#A31D1D",
+          foreground: "color: black;",
+        },
+        background: "#A31D1D",
+        foreground: "#ffffff",
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate],
+} satisfies Config;
